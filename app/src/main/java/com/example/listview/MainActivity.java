bb6acbb645303f0265812c7881f205c6a45d0e8a
batool fatima <batool12547@gmail.com>
2022-05-29 23:17:45 +0500

    second

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -11,9 +11,8 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity
-{
-    ListView list ;
+public class MainActivity extends AppCompatActivity {
+    ListView list;
     Button btn;
     EditText text;
     ArrayList<String> array = new ArrayList<String>();
@@ -25,7 +24,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn);
         list = findViewById(R.id.listview);
-        text= findViewById(R.id.txt);
+        text = findViewById(R.id.txt);
         array.add("A");
         array.add("B");
         array.add("C");
@@ -33,35 +32,13 @@ public class MainActivity extends AppCompatActivity
         array.add("E");
         array.add("F");
         array.add("G");
-        array.add("H");
-        array.add("I");
-        array.add("J");
-        array.add("K");
-        array.add("L");
-//        array.add("M");
-//        array.add("N");
-//        array.add("O");
-//        array.add("P");
-//        array.add("Q");
-//        array.add("R");
-//        array.add("S");
-//        array.add("T");
-//        array.add("U");
-//        array.add("V");
-//        array.add("W");
-//        array.add("X");
-//        array.add("Y");
-         adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,array);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, array);
         list.setAdapter(adapter);
     }
 
-    public void onClickfunction(View v)
-    {
+    public void onClickfunction(View v) {
         array.add(text.getText().toString());
         adapter.notifyDataSetChanged();
     }
-//    btn.setOnClickListener(new View.OnClickListener)
-//    {
-//        void onClick
-//    }
 }
